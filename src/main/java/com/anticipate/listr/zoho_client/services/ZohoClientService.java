42c65a072de2b13b8fcd1aa14f035ca493d8b3cf
CooperP1309 <cooperpeek1309@gmail.com>
2026-07-29 23:19:48 +1000

    FIXED: Missing org Id error when calling /token
    WHY IN THE WORLD DOES 2OAUTH ONLY WORK WHEN I USED THE VALUE OF scope WITH THE soid KEY AGGGGHHHHH
    
    THANK GOODNESS I ACCIDENTALLY SOLVED IT ACOUPLE OF MONTHS AGO

--- src/main/java/com/anticipate/listr/zoho_client/services/ZohoClientService.java
+++ src/main/java/com/anticipate/listr/zoho_client/services/ZohoClientService.java
@@ -1,5 +1,6 @@
 package com.anticipate.listr.zoho_client.services;
 
+import org.springframework.http.MediaType;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
 import org.springframework.web.client.RestClient;
@@ -11,19 +12,23 @@ public class ZohoClientService {
 
     private final String preTokenBody;
 
+    private final String tokenHost;
+
     // Constructor preceeds @Value injection - Hence the constructor injection
     public ZohoClientService(
             @Value("${zoho.client.id}") String clientId,
             @Value("${zoho.client.secret}") String clientSecret,
-            @Value("${zoho.client.scope}") String scope,
-            @Value("${zoho.client.host}") String redirectHost,
-            @Value("${zoho.client.zsoid}") String zsoid) {
+            @Value("${zoho.client.zsoid}") String zsoid,
+            @Value("${zoho.client.host}") String tokenHost) {
         
         this.restClient = RestClient.create();
 
         this.preTokenBody = "grant_type=client_credentials&client_id=" + clientId +
-                            "&client_secret=" + clientSecret + "&scope=Desk.tickets.READ&soid="
-                            + zsoid;
+                            "&client_secret=" + clientSecret + 
+                            "&scope=Desk.tickets.READ&soid=Desk.tickets.READ";
+
+
+        this.tokenHost = tokenHost;
     }
 
     public String getExample() {
@@ -34,16 +39,12 @@ public class ZohoClientService {
     }
 
     public String getZohoAccessToken() {
-        
-        
-        return preTokenBody;
 
-        /*
         return restClient.post()
-                .uri("https://accounts.zoho.com.au/oauth/v2/token")
-                //.body(body)
+                .uri(tokenHost + "/oauth/v2/token")
+                .contentType(MediaType.APPLICATION_FORM_URLENCODED)
+                .body(preTokenBody)
                 .retrieve()
                 .body(String.class);
-        */
     }
 }
